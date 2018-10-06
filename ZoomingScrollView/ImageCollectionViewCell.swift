4e9e99d450aa393649a533643c953d386024a630
Harshal Valanda <harshalvalanda@Harshals-MacBook-Pro.local>
2018-10-06 20:44:03 +0530

    Update

---
+++ ZoomingScrollView/ImageCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ImageCollectionViewCell.swift
+//  ZoomingScrollView
+//
+//  Created by MAC-4 on 10/6/17.
+//  Copyright © 2017 Prismetric-MD2. All rights reserved.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ ZoomingScrollView/ZoomingViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ZoomingViewController.swift
+//  ZoomingScrollView
+//
+//  Created by MAC-4 on 10/6/17.
+//  Copyright © 2017 Prismetric-MD2. All rights reserved.
+//
+
+import UIKit
+
+class ZoomingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
